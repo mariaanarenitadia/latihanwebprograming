3895a4f85bfa9218c0b8a42657f61cdcc0ac9a76
mariaanarenitadia <127914113+mariaanarenitadia@users.noreply.github.com>
2023-03-24 12:46:44 +0800

    Renitadia

---
+++ reni2201010355/sesi II/latihan02.php
@@ -0,0 +1,9 @@
+<?php
+    echo "<h3>latihan kondisi pada php</h3>";
+    $angka = 2;
+
+    if($angka > 3){
+            echo "nilai angka lebih dari 3";
+    }else{
+            echo "nilai angka kurang dari tiga atau sama dengan 3";
+    } 
